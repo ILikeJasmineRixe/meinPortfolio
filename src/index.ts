87cc7153e915b01496df563ba0c490dbec3f74d3
ILikeJasmineRixe <ilikejasminerice@gmail.com>
2026-05-20 12:49:04 -0500

    fixed webserver functionality and implemented bootstrap

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,17 @@ const staticPath = "public";
 const server = Bun.serve({
   port: 6969,
   fetch(req) {
-    /*
-    const filePath = staticPath + new URL(req.url).pathname;
-    const file = Bun.file(filePath);
-    return new Response(file)
-    */
       const url = new URL(req.url);
-      
+      const file = Bun.file(`./${staticPath}${url.pathname}`);
+
       if (url.pathname === "/") {
         return new Response(Bun.file(`./${staticPath}/index.html`));
       }
-      
-      if (url.pathname.startsWith(staticPath)) {
-        const file = Bun.file(url.pathname.slice(1));
-        if (file.size === 0) {
-          throw new Error("File not found");
-        }
+
+      if (file.size > 0) {
         return new Response(file);
       }
-      
+
       return new Response(Bun.file(`./${staticPath}/404.html`));
   },
 });
